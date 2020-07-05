db837af5bae134498d315bc557d3947c6f84dbd2
Fahim <55667286+fahim2k29@users.noreply.github.com>
2020-07-05 21:18:38 +0600

    order review

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import fakeData from '../../fakeData';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
+    const [cart, setCart] = useState([]);
+
+    const removeProduct = (productKey) => {
+        const newCart = cart.filter(pd => pd.key !== productKey);
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
+
+
+    useEffect(() =>{
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        
+        const cartProducts =  productKeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        setCart(cartProducts);
+    }, [])
     return (
         <div>
-            <h3>This is Review</h3>
+            <h3>Cart Items: {cart.length}</h3>
+        {
+            cart.map(pd => <ReviewItem
+                key= {pd.key}
+                removeProduct = {removeProduct}
+                product={pd}></ReviewItem>)
+        }
+
         </div>
     );
 };
